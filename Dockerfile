32f9e1d650655ae8c4abfe87a2f4e9fb00f7958b
Gomnonix <lee20481997@gmail.com>
2024-08-12 19:23:31 +0900

    ruff mypy setting

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,29 @@
-FROM python:3.12-alpine3.19
-
-# LABEL maintainer="gomnonix"
+FROM python:3.12-alpine3.20
 
 ENV PYTHONUNBUFFERED 1
 
 COPY ./requirements.txt /tmp/requirements.txt
+COPY ./requirements.dev.txt /tmp/requirements.dev.txt
 COPY ./customk /app
 
 WORKDIR /app
 EXPOSE 8000
 
-ARG DEV=false
+ARG DEV=true
 
 RUN apk add --update --no-cache libffi-dev
 RUN python -m venv /py && \
     /py/bin/pip install --upgrade pip && \
-    /py/bin/pip install -r /tmp/requirements.txt && \
-    rm -rf /tmp
-
-
+    if [ $DEV = "true" ]; \
+        then /py/bin/pip install -r /tmp/requirements.dev.txt ; \
+    else \
+        /py/bin/pip install -r /tmp/requirements.txt ; \
+    fi && \
+    rm -rf /tmp && \
+    adduser \
+        --disabled-password \
+        --no-create-home \
+        django-user
 ENV PATH="/py/bin:$PATH"
+
+USER django-user
--- customk/users/models.py
+++ customk/users/models.py
@@ -1,44 +1,40 @@
-from django.contrib.auth.models import (
-    AbstractBaseUser,
-    PermissionsMixin,
-    BaseUserManager
-)
-from django.db import models
 from common.models import CommonModel
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
 
 
 class UserManager(BaseUserManager):
-    # 일반 유저 생성 함수
-    def create_user(self, email, password):
-        if not email:
-            raise ValueError("이메일이 필요합니다")
+	# 일반 유저 생성 함수
+	def create_user(self, email, password):
+		if not email:
+			raise ValueError("이메일이 필요합니다")
 
-        user = self.model(email=email)
-        user.set_password(password)
-        user.save()
+		user = self.model(email=email)
+		user.set_password(password)
+		user.save()
 
-        return user
+		return user
 
-    def create_superuser(self, email, password):
-        user = self.create_user(email, password)
+	def create_superuser(self, email, password):
+		user = self.create_user(email, password)
 
-        user.is_superuser = True
-        user.is_staff = True
-        user.save()
+		user.is_superuser = True
+		user.is_staff = True
+		user.save()
 
-        return user
+		return user
 
 
-class User(AbstractBaseUser, PermissionsMixin):
-    email = models.CharField(max_length=255, unique=True)
-    nickname = models.CharField(max_length=20)
+class User(AbstractBaseUser, PermissionsMixin, CommonModel):
+	email = models.CharField(max_length=255, unique=True)
+	nickname = models.CharField(max_length=20)
 
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+	is_active = models.BooleanField(default=True)
+	is_staff = models.BooleanField(default=False)
 
-    USERNAME_FIELD = 'email'
+	USERNAME_FIELD = "email"
 
-    objects = UserManager()
+	objects = UserManager()
 
-    def __str__(self) -> str:
-        return f'email {self.email}, nickname: {self.nickname}'
+	def __str__(self) -> str:
+		return f"email {self.email}, nickname: {self.nickname}"
