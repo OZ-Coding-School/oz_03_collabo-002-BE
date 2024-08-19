5e4862f087d2bdf7ca3765043f6c57c45dcc09d7
rbwo552 <rbwo552@naver.com>
2024-08-19 22:11:32 +0900

    update settings.py

--- customk/config/settings.py
+++ customk/config/settings.py
@@ -67,10 +67,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
