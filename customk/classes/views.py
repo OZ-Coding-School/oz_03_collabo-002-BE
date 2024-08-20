e7e7056d58f5552210eab14c63c2a381c7a3c6a2
Gomnonix <lee20481997@gmail.com>
2024-08-20 14:15:46 +0900

    클래스 상태 메세지 추가

--- customk/classes/views.py
+++ customk/classes/views.py
@@ -9,23 +9,47 @@ class ClassListView(APIView):
     def get(self, request, *args, **kwargs):
         classes = Class.objects.all()
         serializer = ClassSerializer(classes, many=True)
-        return Response(serializer.data)
+        response_data = {
+            "status": "success",
+            "message": "Event fetched successfully",
+            "data": serializer.data,
+        }
+        return Response(response_data, status=200)
 
     def post(self, request, *args, **kwargs):
         serializer = ClassSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=201)
+            response_data = {
+                "status": "success",
+                "message": "Event created successfully",
+                "data": serializer.data,
+            }
+            return Response(response_data, status=201)
         return Response(serializer.errors, status=400)
 
     def patch(self, request, *args, **kwargs):
         serializer = ClassSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=200)
+            response_data = {
+                "status": "success",
+                "message": "Event updated successfully",
+                "data": serializer.data,
+            }
+            return Response(response_data, status=200)
+        return Response(serializer.errors, status=400)
 
     def delete(self, request, *args, **kwargs):
-        serializer = ClassSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=204)
+        class_id = request.data.get("id", None)
+        if class_id is None:
+            return Response({"status": "error", "message": "Class ID not provided"}, status=400)
+
+        try:
+            class_instance = Class.objects.get(id=class_id)
+            class_instance.delete()
+            return Response(
+                {"status": "success", "message": "Event deleted successfully"}, status=204
+            )
+        except Class.DoesNotExist:
+            return Response({"status": "error", "message": "Event not found"}, status=404)
