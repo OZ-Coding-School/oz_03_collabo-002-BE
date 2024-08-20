136985497c6420891473c7a9b5f73670a2faf8e8
Gomnonix <lee20481997@gmail.com>
2024-08-20 14:14:52 +0900

    의미없는 커밋

--- customk/classes/consumers.py
+++ customk/classes/consumers.py
@@ -4,20 +4,12 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        await self.channel_layer.group_add(
-            'notification_group',
-            self.channel_name
-        )
+        await self.channel_layer.group_add("notification_group", self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
-            'notification_group',
-            self.channel_name
-        )
+        await self.channel_layer.group_discard("notification_group", self.channel_name)
 
     async def send_notification(self, event):
-        notification = event['notification']
-        await self.send(text_data=json.dumps({
-            'notification': notification
-        }))
+        notification = event["notification"]
+        await self.send(text_data=json.dumps({"notification": notification}))
