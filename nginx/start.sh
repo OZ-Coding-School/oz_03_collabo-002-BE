2ec0fa79fac647322ef3b6b17bfec06018fa503d
rbwo552 <rbwo552@naver.com>
2024-08-20 12:54:58 +0900

    update nginx file

--- nginx/start.sh
+++ nginx/start.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+until nc -z $APP_HOST $APP_PORT; do
+    echo "Waiting for the 'app' service..."
+    sleep 1
+done
+echo "SERVER_NAME is set to: ${SERVER_NAME}" >&2
+
+echo "Contents of nginx.conf.template:" >&2
+cat /etc/nginx/nginx.conf.template >&2
+
 envsubst '${SERVER_NAME}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf \
 >> /etc/nginx/script-reuslt 2>&1
 
@@ -10,4 +19,5 @@ nginx -t >> /etc/nginx/script-reuslt 2>&1
 cat /etc/nginx/nginx.conf.template >> /etc/nginx/script-reuslt 2>&1
 cat /etc/nginx/conf.d/default.conf >> /etc/nginx/script-reuslt 2>&1
 
+echo "Starting nginx..." >&2
 exec nginx -g 'daemon off;' >> /etc/nginx/script-reuslt 2>&1
