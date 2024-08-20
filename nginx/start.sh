cb738c1272e3898669ad0ee5c19108352679de0c
rbwo552 <rbwo552@naver.com>
2024-08-20 12:31:50 +0900

    update nginx file

--- nginx/start.sh
+++ nginx/start.sh
@@ -2,14 +2,12 @@
 
 set -e
 
-touch /tmp/script-reuslt
-
 envsubst '${SERVER_NAME}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf \
->> /tmp/script-reuslt 2>&1
+>> /etc/nginx/script-reuslt 2>&1
 
-nginx -t >> /tmp/script-reuslt 2>&1
+nginx -t >> /etc/nginx/script-reuslt 2>&1
 
-cat /etc/nginx/nginx.conf.template >> /tmp/script-reuslt 2>&1
-cat /etc/nginx/conf.d/default.conf >> /tmp/script-reuslt 2>&1
+cat /etc/nginx/nginx.conf.template >> /etc/nginx/script-reuslt 2>&1
+cat /etc/nginx/conf.d/default.conf >> /etc/nginx/script-reuslt 2>&1
 
-exec nginx -g 'daemon off;' >> /tmp/script-reuslt 2>&1
+exec nginx -g 'daemon off;' >> /etc/nginx/script-reuslt 2>&1
