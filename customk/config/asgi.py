88a518212f47ce85564c86821fcede5321730617
Gomnonix <lee20481997@gmail.com>
2024-08-20 14:13:42 +0900

    의미없는 커밋

--- customk/config/asgi.py
+++ customk/config/asgi.py
@@ -4,14 +4,11 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from classes import routing  # classes 앱의 routing.py를 포함합니다
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            routing.websocket_urlpatterns
-        )
-    ),
-})
-
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns)),
+    }
+)
