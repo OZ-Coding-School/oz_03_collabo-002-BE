6cc8f4145010702165814ad26cf6cb2e65b279a1
Gomnonix <lee20481997@gmail.com>
2024-08-28 17:31:23 +0900

    문의 관리자 페이지 등록

--- customk/questions/admin.py
+++ customk/questions/admin.py
@@ -1,1 +1,19 @@
 from django.contrib import admin
+from .models import Question
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("user_id", "class_id", "question_title", "created_at", "updated_at")
+    list_filter = ("class_id", "created_at", "user_id")
+    search_fields = ("user_id__name", "class_id__title", "question", "question_title")
+    fieldsets = (
+        ("기본 정보", {"fields": ("user_id", "class_id")}),
+        ("질문 내용", {"fields": ("question_title", "question")}),
+        ("답변 내용", {"fields": ("answer_title", "answer"), "classes": ("collapse",)}),
+    )
+    readonly_fields = ("created_at", "updated_at")
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.user_id = request.user
+        super().save_model(request, obj, form, change)
