982e9b255b48dab3ae9f5623961057b39fdcf046
rbwo552 <rbwo552@naver.com>
2024-08-29 14:39:21 +0900

    url 수정 및 domain 설정

--- customk/users/urls.py
+++ customk/users/urls.py
@@ -1,17 +1,19 @@
-from django.urls import path
+from django.urls import re_path
 
 from .views import google, kakao, line
 from .views.token import CustomTokenRefreshView, CustomTokenVerifyView
 from .views.user import LoginView, LogoutView, SignupView, UserDetailView
 
 urlpatterns = [
-    path("signup/", SignupView.as_view(), name="signup"),
-    path("login/", LoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout"),
-    path("detail/", UserDetailView.as_view(), name="user-detail"),
-    path("token/refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),
-    path("token/verify/", CustomTokenVerifyView.as_view(), name="token_verify"),
-    path("kakao/callback/", kakao.callback, name="kakao_callback"),
-    path("google/callback/", google.callback, name="google_callback"),
-    path("line/callback/", line.callback, name="line_callback"),
+    re_path(r"^signup/?$", SignupView.as_view(), name="signup"),
+    re_path(r"^login/?$", LoginView.as_view(), name="login"),
+    re_path(r"^logout/?$", LogoutView.as_view(), name="logout"),
+    re_path(r"^detail/?$", UserDetailView.as_view(), name="user-detail"),
+    re_path(
+        r"^token/refresh/?$", CustomTokenRefreshView.as_view(), name="token_refresh"
+    ),
+    re_path(r"^token/verify/?$", CustomTokenVerifyView.as_view(), name="token_verify"),
+    re_path(r"^kakao/callback/?$", kakao.callback, name="kakao_callback"),
+    re_path(r"^google/callback/?$", google.callback, name="google_callback"),
+    re_path(r"^line/callback/?$", line.callback, name="line_callback"),
 ]
