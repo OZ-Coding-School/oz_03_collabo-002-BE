8c3648277c38fd7294f90a10a252134831e4758a
rbwo552 <rbwo552@naver.com>
2024-08-30 16:57:54 +0900

    gunicorn 설정 변경

--- customk/scripts/start-deploy.sh
+++ customk/scripts/start-deploy.sh
@@ -2,4 +2,10 @@
 
 python manage.py migrate
 python manage.py collectstatic --no-input
-exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
+
+exec gunicorn config.wsgi:application \
+  --bind 0.0.0.0:8000 \
+  --workers 5 \
+  --access-logfile logs/gunicorn_access.log \
+  --error-logfile logs/gunicorn_error.log \
+  --timeout 30
