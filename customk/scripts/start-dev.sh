3fc7230756e41b316f6018c2fb6fbe16095d29e8
Gomnonix <lee20481997@gmail.com>
2024-08-30 20:21:41 +0900

    오류 수정

--- customk/scripts/start-dev.sh
+++ customk/scripts/start-dev.sh
@@ -12,6 +12,6 @@ python manage.py collectstatic --no-input
 exec gunicorn config.wsgi:application \
   --bind 0.0.0.0:8000 \
   --workers 5 \
-  --access-logfile logs/app_access.log \
-  --error-logfile logs/app_error.log \
+  --access-logfile - \
+  --error-logfile - \
   --timeout 30
