8694f13880e0c111c458f07c8cf8d03734f0305a
rbwo552 <rbwo552@naver.com>
2024-08-30 18:10:02 +0900

    deploy 스크립트 수정

--- customk/scripts/start-deploy.sh
+++ customk/scripts/start-deploy.sh
@@ -6,6 +6,4 @@ python manage.py collectstatic --no-input
 exec gunicorn config.wsgi:application \
   --bind 0.0.0.0:8000 \
   --workers 5 \
-  --access-logfile logs/gunicorn_access.log \
-  --error-logfile logs/gunicorn_error.log \
   --timeout 30
