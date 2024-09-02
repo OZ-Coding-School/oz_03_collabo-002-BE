f97a07852a21ea72459b061bc894e3dba0a59df7
rbwo552 <rbwo552@naver.com>
2024-09-02 18:20:14 +0900

    pytest ruff ignore 추가

--- customk/favorites/tests/conftest.py
+++ customk/favorites/tests/conftest.py
@@ -1,22 +1,14 @@
 import pytest
 
 from classes.models import Class
-from classes.tests.conftest import sample_class
-from favorites.models import Favorite
 from favorites.services import add_favorite_class
-from users.tests.conftest import (
-    access_token,
-    api_client_with_token,
-    refresh_token,
-    sample_user,
-)
 
 pytestmark = pytest.mark.django_db
 
 
 @pytest.fixture
-def favorite_instance():
-    return Favorite.objects.create(user=sample_user, class_id=sample_class.id)
+def favorite_instance(sample_user, sample_class):
+    return add_favorite_class(user_id=sample_user.id, class_id=sample_class.id)
 
 
 @pytest.fixture
