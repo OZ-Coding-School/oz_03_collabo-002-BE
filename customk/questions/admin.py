1f1f3620829c67ff54904fcf1fa094a40b6a44f2
Gomnonix <lee20481997@gmail.com>
2024-09-03 08:02:20 +0900

    의미없는 커밋

--- customk/questions/admin.py
+++ customk/questions/admin.py
@@ -10,11 +10,12 @@ from .models import Question
 class QuestionAdmin(admin.ModelAdmin):
     list_display = (
         "user_id",
+        "answer_icon",
         "class_id",
         "question_title",
         "created_at",
         "updated_at",
-        "answer_icon",
+
     )
     list_filter = ("class_id", "created_at", "user_id")
     search_fields = ("user_id__name", "class_id__title", "question", "question_title")
@@ -50,16 +51,3 @@ class QuestionAdmin(admin.ModelAdmin):
             )
 
         return super().changelist_view(request, extra_context=extra_context)
-
-    def change_view(
-        self,
-        request: HttpRequest,
-        object_id: str,
-        form_url: str = "",
-        extra_context: Optional[Dict[str, Any]] = None,
-    ) -> HttpResponse:
-        question = self.get_object(request, object_id)
-        if question and question.answer is None:
-            messages.info(request, "이 질문에는 아직 답변이 없습니다.")
-
-        return super().change_view(request, object_id, form_url, extra_context)
