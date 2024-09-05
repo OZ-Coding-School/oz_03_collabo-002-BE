6e3f2a66bf8a13fc010445a1b5f7a4f1e65d1652
Gomnonix <lee20481997@gmail.com>
2024-09-05 17:55:30 +0900

    TODO 작성

--- customk/questions/signals.py
+++ customk/questions/signals.py
@@ -4,6 +4,7 @@ from django.core.cache import cache
 from .models import Question
 
 @receiver(post_save, sender=Question)
+# TODO 관리자 페이지 알림
 def question_post_save(sender, instance, created, **kwargs):
     if created:
         cache.set('new_question_created', True, 300)  # 5분 동안 캐시에 저장
