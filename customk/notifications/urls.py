e4329d1c8d731bb7904489d5ded5711ce1edd38d
Gomnonix <lee20481997@gmail.com>
2024-09-06 17:07:07 +0900

    관리자 페이지 알림

--- customk/notifications/urls.py
+++ customk/notifications/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import unread_notifications_count
 
 urlpatterns = [
-    path('admin/api/unread_notifications_count/', unread_notifications_count, name='unread-notifications-count'),
+    path('unread_count/', unread_notifications_count, name='unread-notifications-count'),
 ]
--- customk/questions/static/question/js/scripts.js
+++ customk/questions/static/question/js/scripts.js
@@ -6,10 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(response => response.json())
         .then(data => {
             if (data.count > 0) {
-                notificationBadge.textContent = data.count;
+                notificationBadge.textContent = '';
                 notificationBadge.style.display = 'inline';
                 notificationIcon.style.color = 'blue';
+            } else {
+                notificationBadge.style.display = 'none';
+                notificationIcon.style.color = '#000';
             }
         })
         .catch(error => console.error('Error fetching notification count:', error));
-});
+});ㅊ
