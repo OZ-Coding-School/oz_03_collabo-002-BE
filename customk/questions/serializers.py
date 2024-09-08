d37e7219eb5b5783cb35d5a7e2a32dde89d8ad37
Gomnonix <lee20481997@gmail.com>
2024-09-09 01:55:56 +0900

    질문 테스트 코드 작성

--- customk/questions/serializers.py
+++ customk/questions/serializers.py
@@ -1,14 +1,11 @@
 from rest_framework import serializers
-
-from users.models import User
-
 from .models import Question
 
 
 class QuestionSerializer(serializers.ModelSerializer):
-    created_at = serializers.DateTimeField(read_only=True)  # created_at 필드 read_only
-    class_id = serializers.IntegerField(read_only=True)  # class_id 필드 read_only
-    user_id = serializers.IntegerField(read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+    class_id = serializers.PrimaryKeyRelatedField(read_only=True)
+    user_id = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = Question
---
+++ customk/questions/tests/conftest.py
@@ -0,0 +1,38 @@
+import pytest
+from users.models import User
+from reviews.models import Review
+from rest_framework.test import APIClient
+from classes.models import Class
+from questions.models import Question
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+@pytest.fixture
+def sample_user():
+    return User.objects.create_user(
+        email="testuser@example.com", password="testpassword"
+    )
+
+
+@pytest.fixture
+def sample_class():
+    return Class.objects.create(
+        title="Sample Class",
+        description="This is a sample class",
+        max_person=10,
+        require_person=5,
+        price=50000,
+        address="서울시 강남구 테헤란로",
+    )
+
+@pytest.fixture
+def question(sample_user, sample_class):
+    return Question.objects.create(
+        user_id=sample_user,
+        class_id=sample_class,
+        question="Test question content",
+        question_title="Test question title"
+    )
